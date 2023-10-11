dffa8acc5389af068f7002a78e9d129f7f360c9a
Aren Sinanian <95931889+arensin8@users.noreply.github.com>
2023-10-11 18:44:38 +0000

    login page added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,8 +9,6 @@ import hamMenu from "../../assets/ham1.png";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const [showLoginMenu, setShowLoginMenu] = useState(false);
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
 
   useEffect(() => {
@@ -26,18 +24,6 @@ const Header = () => {
   const closeMenu = () => {
     setShowMenu(false);
     // Add the body class to disable scrolling
-    document.body.classList.remove("body-no-scroll");
-  };
-
-  const openLogin = () => {
-    setIsLoginOpen(true);
-    // Add the body class to disable scrolling
-    document.body.classList.add("body-no-scroll");
-  };
-
-  const closeLogin = () => {
-    setIsLoginOpen(false);
-    // Remove the body class to re-enable scrolling
     document.body.classList.remove("body-no-scroll");
   };
 
@@ -125,35 +111,6 @@ const Header = () => {
           </button>
         </Link>
 
-        {isLoginOpen && (
-          <div
-            className="loginCover"
-            style={{ visibility: showLoginMenu ? "visible" : "hidden" }}
-          >
-            <img
-              src={close}
-              className="close"
-              onClick={() => {
-                setShowLoginMenu((showLoginMenu) => !showLoginMenu);
-                closeLogin();
-              }}
-              alt="close"
-            />
-            <h1>Login</h1>
-            <div className="loginContainer">
-              <input type="email" placeholder="Enter your email" />
-              <input type="username" placeholder="Enter your username" />
-              <ReCAPTCHA
-                sitekey="Your client site key"
-                onChange={onChange}
-                className="captcha"
-              />
-              <a href="/login" className="loginBtn" onClick={toggleBars}>
-                Login
-              </a>
-            </div>
-          </div>
-        )}
         <div
           className="bars"
           onClick={(toggleBars, openMenu)}
