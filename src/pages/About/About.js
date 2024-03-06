a763cac2f792bd376d672504df330b6119587d5a
Aren Sinanian <95931889+arensin8@users.noreply.github.com>
2024-03-06 19:39:21 +0000

    hero section fixings

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -13,28 +13,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PresentationControls, Stage } from "@react-three/drei";
 
 const About = () => {
-  return (
-    <>
-      <Canvas
-        dpr={[1, 2]}
-        shadows
-        camera={{ fov: 45 }}
-        style={{ position: "absolute" }}
-      >
-        <color attach="background" args={["#101010"]} />
-        {/* <PresentationControls
-          speed={1.5}
-          global
-          zoom={0.5}
-          polar={[-0.1, Math.PI / 4]}
-        >
-          <Stage environment={"sunset"}>
-            <Model scale={0.01} />
-          </Stage>
-        </PresentationControls> */}
-      </Canvas>
-    </>
-  );
+  return <></>;
 };
 
 export default About;
